49adaf88d4850dc33410d4ba29e5b553a61d127d
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2026-02-02 18:59:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Store two numbers of your choice in two different variables. 
+# Now perform all the Arithmetic operations on those two numbers 
+# and print the results.
+a=15
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
